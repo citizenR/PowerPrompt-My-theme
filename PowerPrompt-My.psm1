290b5c990c765741641e51e5aafa1d1a5b8e5549
R <rao@citizenr.github.com>
2020-03-01 13:54:13 +0800

    Add project files.

---
+++ PowerPrompt-My.psm1
@@ -0,0 +1,71 @@
+#requires -Version 2 -Modules posh-git
+
+function Write-Theme {
+
+    param(
+        [bool]
+        $lastCommandFailed,
+        [string]
+        $with
+    )
+
+    $lastColor = $sl.Colors.PromptBackgroundColor
+    $user = [System.Environment]::UserName
+    $userSymbol = " > "
+    $adminSymbol = " $ "
+
+    #check the last command state and indicate if failed
+    #check for elevated prompt
+    if(Test-Administrator){
+        if ($lastCommandFailed) {
+            $prompt = Write-Prompt -Object $adminSymbol -ForegroundColor $sl.Colors.PromptForegroundColor -BackgroundColor $sl.Colors.CommandFailedIconForegroundColor
+        } else {
+            $prompt = Write-Prompt -Object $adminSymbol -ForegroundColor $sl.Colors.PromptWarnColorD -BackgroundColor $sl.Colors.PromptWarnColorY
+        }
+    }else{
+        if ($lastCommandFailed) {
+            $prompt = Write-Prompt -Object $userSymbol -ForegroundColor $sl.Colors.PromptForegroundColor -BackgroundColor $sl.Colors.CommandFailedIconForegroundColor
+        } else {
+            $prompt = Write-Prompt -Object $userSymbol -ForegroundColor $sl.Colors.SessionInfoForegroundColor -BackgroundColor $sl.Colors.SessionInfoBackgroundColor
+        }
+    }
+
+
+    if (Test-VirtualEnv) {
+        $prompt += Write-Prompt -Object " (venv) $(Get-VirtualEnvName) " -ForegroundColor $sl.Colors.VirtualEnvForegroundColor -BackgroundColor $sl.Colors.VirtualEnvBackgroundColor
+    }
+
+    # Writes the drive portion
+    $path = ' ' + (Split-Path -leaf -path (Get-Location)) + ' '
+    $prompt += Write-Prompt -Object $path -ForegroundColor $sl.Colors.PromptForegroundColor -BackgroundColor $sl.Colors.PromptBackgroundColor
+    
+    $status = Get-VCSStatus
+    if ($status) {
+        $themeInfo = Get-VcsInfo -status ($status)
+        $lastColor = $themeInfo.BackgroundColor
+        $prompt += Write-Prompt -Object " $($themeInfo.VcInfo) " -BackgroundColor $lastColor -ForegroundColor $sl.Colors.GitForegroundColor
+    }
+
+    if ($with) {
+        $prompt += Write-Prompt -Object " $($with.ToUpper()) " -BackgroundColor $sl.Colors.WithBackgroundColor -ForegroundColor $sl.Colors.WithForegroundColor
+        $lastColor = $sl.Colors.WithBackgroundColor
+    }
+
+    # Writes the postfix to the prompt
+    $prompt += ' '
+    $prompt
+}
+
+$sl = $global:ThemeSettings #local settings
+$sl.PromptSymbols.SegmentForwardSymbol = ""
+$sl.Colors.SessionInfoBackgroundColor = [ConsoleColor]::DarkGray
+$sl.Colors.PromptForegroundColor = [ConsoleColor]::White
+$sl.Colors.PromptSymbolColor = [ConsoleColor]::White
+$sl.Colors.PromptWarnColorY = [ConsoleColor]::Yellow
+$sl.Colors.PromptWarnColorD = [ConsoleColor]::DarkGray
+$sl.Colors.PromptHighlightColor = [ConsoleColor]::DarkBlue
+$sl.Colors.GitForegroundColor = [ConsoleColor]::DarkGray
+$sl.Colors.WithForegroundColor = [ConsoleColor]::White
+$sl.Colors.WithBackgroundColor = [ConsoleColor]::DarkRed
+$sl.Colors.VirtualEnvBackgroundColor = [System.ConsoleColor]::Red
+$sl.Colors.VirtualEnvForegroundColor = [System.ConsoleColor]::White
